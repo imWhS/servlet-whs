ba98fdbfd36c4f135e5b790320950b090f3af635
Wonhee Son <dnjsgml1230@gmail.com>
2023-07-27 22:14:06 +0900

    프론트 컨트롤러 패턴 도입

---
+++ src/main/java/hellowhs/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,20 @@
+package hellowhs.servlet.web.frontcontroller.v1.controller;
+
+import hellowhs.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+
+}
