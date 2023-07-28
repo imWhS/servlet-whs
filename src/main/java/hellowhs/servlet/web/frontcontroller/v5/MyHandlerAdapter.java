5a5c624c78e994ff8dbb8f546170dcbd2d188e59
Wonhee Son <dnjsgml1230@gmail.com>
2023-07-28 12:12:11 +0900

    어댑터 패턴 도입

---
+++ src/main/java/hellowhs/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -0,0 +1,13 @@
+package hellowhs.servlet.web.frontcontroller.v5;
+
+import hellowhs.servlet.web.frontcontroller.ModelView;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface MyHandlerAdapter {
+
+    boolean supports(Object handler);
+
+    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler);
+
+}
