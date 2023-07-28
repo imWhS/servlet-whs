1af560058b81d6ba5574487854f989e89b47ca68
Wonhee Son <dnjsgml1230@gmail.com>
2023-07-28 10:36:25 +0900

    컨트롤러의 서블릿 종속성 제거

---
+++ src/main/java/hellowhs/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -0,0 +1,11 @@
+package hellowhs.servlet.web.frontcontroller.v3;
+
+import hellowhs.servlet.web.frontcontroller.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+
+    ModelView process(Map<String, String> paramMap);
+
+}
